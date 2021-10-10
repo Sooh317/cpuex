b8f2af4c2ecfa5d9149f24c7e24a9f5f6ec3b4b6
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-10-10 17:46:06 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
+#include <map>
 #include "struct.h"
 #include "memory.hpp"
 #include "decode.hpp"
 #include "util.hpp"
 
-int main(){
+int main(int argc, char* argv[]){
+    CPU cpu;
+    MEMORY mem;
+    std::map<std::string, int> label;
+
+    decode(argv[1], mem, label);
     return 0;
 }
