3b4dcd586e6fe985e0cc98a27c21f3fd09c73818
Sooh317 <wangshuo120731@gmail.com>
2021-10-15 19:57:41 +0900

    issues when switching mem and submem

--- memory.cpp
+++ memory.cpp
@@ -3,7 +3,7 @@
 
 
 void init_memory(int argc, char *argv[], MEMORY& mem, MEMORY& submem, std::map<std::string, int>&lbl, std::map<std::string, int>& sublbl){
-    int tmp = collect_label(argv[1], lbl);
+    collect_label(argv[1], lbl);
     if(argc >= 3) collect_label(argv[2], sublbl);
     decode(argv[1], mem, lbl, sublbl);
     if(argc >= 3) decode(argv[2], submem, sublbl, lbl);
