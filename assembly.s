e3cc3d408a01e210925ef10c902a41f1b0d8b54a
Sooh317 <wangshuo120731@gmail.com>
2021-10-26 15:00:47 +0900

    translator almost done, need some fix for branch address

---
+++ assembly.s
@@ -0,0 +1,4 @@
+addi  r2, 0, 3
+mfspr    r31, 8
+stw r31, 4(r3)
+addi    r3, r3, 8
--- option.hpp
+++ option.hpp
@@ -53,6 +53,7 @@ struct option_t{
         if(binary) std::cerr << "reading binary" << std::endl;
         if(assembly) std::cerr << "### invalid option ###\nreading assembly.s" << std::endl;
         else std::cerr << "reading assembly" << std::endl;
+        std::cerr << std::endl;
     }
 };
 using OPTION = option_t;
