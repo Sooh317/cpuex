39c0c0419888e05f6f0307bfb02ef702b93cf00c
Sooh317 <wangshuo120731@gmail.com>
2021-10-30 19:23:07 +0900

    cache needs testing

--- cache.hpp
+++ cache.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include <struct.hpp>
-#include <util.hpp>
 #include <vector>
+#include "struct.hpp"
+#include "util.hpp"
 
 
 #define CACHE_TAG_SIZE 18
@@ -18,17 +18,16 @@ struct cache_t{
     };
 private:
     const int k = CACHE_LINE_SIZE / 32;
-    int hit, miss;
+    int hit, miss, write_back;
     std::vector<state_t> state;
     std::vector<std::vector<int>> data;
 public:
-    cache_t():hit(0), miss(0){
-        data.resize(CACHE_LINE_NUM, std::vector<INSTR>(k));
+    cache_t():hit(0), miss(0), write_back(0){
+        data.resize(CACHE_LINE_NUM, std::vector<int>(k));
         state.resize(CACHE_LINE_NUM);
     }
-    void find(bool lw, int32_t addr, const MEMORY& mem){
+    void find(bool lw, int32_t addr, MEMORY& mem, int d = 0){
         int offset = addr & MASK5;
-        int base = offset >> 2;
         int index = (addr >> CACHE_OFFSET_SIZE) & MASK4;
         int tag = (addr >> (CACHE_INDEX_SIZE + CACHE_OFFSET_SIZE)) & MASK18;
         addr = addr & ((~0) ^ MASK5); // 下のoffset size分だけ0にクリア
@@ -38,10 +37,33 @@ public:
                 state[index].valid = 1;
                 state[index].tag = tag;
                 for(int i = 0; i < k; i++){
-                    data[base + i] = mem.data[(addr >> 2) + i];
+                    data[index][i] = mem.data[((addr >> 2) & MASK3) + i]; // line size により maskの長さが変わるので注意
                 }
             }
             else ++hit;
+        }
+        else{
+            if(state[index].valid && state[index].tag == tag){
+                ++hit;
+                state[index].dirty = 1;
+                data[index][(offset >> 2)] = d;
+            }
+            else{
+                std::swap(tag, state[index].tag);
+                state[index].valid = 1;
+                if(state[index].dirty){
+                    write_back++;
+                    for(int i = 0; i < k; i++){
+                        mem.data[((addr >> 2) & MASK3) + i] = data[index][i]; // line size により maskの長さが変わるので注意
+                    }
+                }
+                // 仕様による
+                state[index].dirty = 1;
+                for(int i = 0; i < k; i++){
+                    data[index][i] = mem.data[((addr >> 2) & MASK3) + i];
+                }
+                data[index][offset >> 2] = d;
+            }
         }
     }
 };
