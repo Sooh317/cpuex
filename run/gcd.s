553747627fedd4849094c619e6cbb02aa7b6b6ab
Sooh317 <wangshuo120731@gmail.com>
2021-12-01 15:04:44 +0900

    fcfiw needs to be implemented, 65535 treated as -1

--- run/gcd.s
+++ run/gcd.s
@@ -13,38 +13,27 @@ ble_else.17:
 	b	gcd.7				# 3
 ble_else.18:
 	sub	r2, r2, r5				# 3
-	mr	r29, r5				# 3
+	mr	r30, r5				# 3
 	mr	r5, r2				# 3
-	mr	r2, r29				# 3
+	mr	r2, r30				# 3
 	b	gcd.7				# 3
 _min_caml_start: # main entry point
-	mfspr	r0, 8
-	stw	r30, -8(r1)
-	stw	r31, -4(r1)
-	stw	r0, 8(r1)
-	stwu	r1, -96(r1)
-#	main program starts
 	addi	r2, 0, 21600
 	addis	r5, 0, 5				# 4
 	ori	r5, r5, 9820				# 4
 	mfspr	r31, 8				# 4
-	stw	r31, 4(r3)				# 4
-	addi	r3, r3, 8				# 4
+	stw	r31, 0(r3)				# 4
+	addi	r3, r3, 4				# 4
 	bl	gcd.7				# 4
-	addi	r3, r3, -8				# 4
-	lwz	r31, 4(r3)				# 4
+	addi	r3, r3, -4				# 4
+	lwz	r31, 0(r3)				# 4
 	mtspr	8, r31				# 4
 	mfspr	r31, 8				# 4
-	stw	r31, 4(r3)				# 4
-	addi	r3, r3, 8				# 4
+	stw	r31, 0(r3)				# 4
+	addi	r3, r3, 4				# 4
 	bl	min_caml_print_int				# 4
-	addi	r3, r3, -8				# 4
-	lwz	r31, 4(r3)				# 4
+	addi	r3, r3, -4				# 4
+	lwz	r31, 0(r3)				# 4
 	mtspr	8, r31				# 4
 #	main program ends
-	lwz	r1, 0(r1)
-	lwz	r0, 8(r1)
-	mtspr	8, r0
-	lwz	r30, -8(r1)
-	lwz	r31, -4(r1)
-	blr
+	halt
