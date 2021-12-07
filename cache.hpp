82267f3f008e671f33a17c85e2e535469491d006
Sooh317 <wangshuo120731@gmail.com>
2021-12-07 11:35:33 +0900

    cache prepared

--- cache.hpp
+++ cache.hpp
@@ -17,52 +17,66 @@ struct cache_t{
         state_t():dirty(0), accessed(0), tag(0){}
     };
 private:
-    const int k = CACHE_LINE_SIZE / 32;
+    const int width = CACHE_LINE_SIZE / 32;
     int hit, miss, write_back;
     std::vector<state_t> state;
     std::vector<std::vector<DATA>> data;
-public:
-    cache_t():hit(0), miss(0), write_back(0){
-        data.resize(CACHE_LINE_NUM, std::vector<DATA>(k));
-        state.resize(CACHE_LINE_NUM);
-    }
-    void find(bool lw, int32_t addr, MEMORY& mem, DATA d = DATA{0}){
-        int offset = addr & bitmask(5);
-        int index = (addr >> CACHE_OFFSET_SIZE) & bitmask(4);
-        int tag = (addr >> (CACHE_INDEX_SIZE + CACHE_OFFSET_SIZE)) & bitmask(18);
-        addr = addr & ((~0) ^ bitmask(5)); // 下のoffset size分だけ0にクリア
-        if(lw){
-            if(state[index].tag != tag){
-                ++miss;
-                state[index].tag = tag;
-                for(int i = 0; i < k; i++){
-                    data[index][i] = mem.data[((addr >> 2) & bitmask(3)) + i]; // line size により maskの長さが変わるので注意
-                }
-            }
-            else ++hit;
+
+    void sw(int32_t addr, MEMORY& mem, DATA d){
+        int offset = addr & bitmask(CACHE_OFFSET_SIZE);
+        int index = (addr >> CACHE_OFFSET_SIZE) & bitmask(CACHE_INDEX_SIZE);
+        int tag = (addr >> (CACHE_INDEX_SIZE + CACHE_OFFSET_SIZE)) & bitmask(CACHE_TAG_SIZE);
+        addr = addr & ((~0) ^ bitmask(CACHE_OFFSET_SIZE)); // 下のoffset size分だけ0にクリア
+        if(state[index].tag == tag){
+            ++hit;
+            state[index].dirty = 1;
+            data[index][(offset >> 2)] = d;
         }
         else{
-            if(state[index].tag == tag){
-                ++hit;
-                state[index].dirty = 1;
-                data[index][(offset >> 2)] = d;
+            std::swap(tag, state[index].tag);
+            if(state[index].dirty){
+                write_back++;
+                for(int i = 0; i < width; i++){
+                    mem.data[(addr >> 2) + i] = data[index][i]; // line size により maskの長さが変わるので注意
+                }
             }
-            else{
-                std::swap(tag, state[index].tag);
-                if(state[index].dirty){
-                    write_back++;
-                    for(int i = 0; i < k; i++){
-                        mem.data[((addr >> 2) & bitmask(3)) + i] = data[index][i]; // line size により maskの長さが変わるので注意
-                    }
-                }
-                // 仕様による
-                state[index].dirty = 1;
-                for(int i = 0; i < k; i++){
-                    data[index][i] = mem.data[((addr >> 2) & bitmask(3)) + i];
-                }
-                data[index][offset >> 2] = d;
+            // 仕様による
+            state[index].dirty = 1;
+            for(int i = 0; i < width; i++){
+                data[index][i] = mem.data[(addr >> 2) + i];
+            }
+            data[index][offset >> 2] = d;
+        }
+    }
+
+public:
+    cache_t():hit(0), miss(0), write_back(0){
+        data.resize(CACHE_LINE_NUM, std::vector<DATA>(width));
+        state.resize(CACHE_LINE_NUM);
+    }
+    // addr is a multiple of 4
+    void lw(int32_t addr, MEMORY& mem){
+        int index = (addr >> CACHE_OFFSET_SIZE) & bitmask(CACHE_INDEX_SIZE);
+        int tag = (addr >> (CACHE_INDEX_SIZE + CACHE_OFFSET_SIZE)) & bitmask(CACHE_TAG_SIZE);
+        addr = addr & ((~0) ^ bitmask(CACHE_OFFSET_SIZE)); // 下のoffset size分だけ0にクリア
+        if(state[index].tag != tag){
+            ++miss;
+            state[index].tag = tag;
+            for(int i = 0; i < width; i++){
+                data[index][i] = mem.data[(addr >> 2) + i]; // line size により maskの長さが変わるので注意
             }
         }
+        else ++hit;
+    }
+    void swf(int32_t addr, MEMORY& mem, float f){
+        DATA d;
+        d.f = f;
+        sw(addr, mem, d);
+    }
+    void swi(int32_t addr, MEMORY& mem, int i){
+        DATA d;
+        d.i = i;
+        sw(addr, mem, d);
     }
 };
 using CACHE = cache_t;
