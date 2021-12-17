f6b24dbbb19cf5082c1d5f3c8ea58e9fb78147b9
Sooh317 <wangshuo120731@gmail.com>
2021-12-17 17:37:31 +0900

    fixed fpu

--- cpu.hpp
+++ cpu.hpp
@@ -9,6 +9,7 @@
 // stack pointer　初期値 -> 2^25 - 1
 void init_cpu(CPU& cpu, OPTION& option, std::map<std::string, int> &lbl, int sp = (DATA_SIZE - 1) * 4){
     cpu.gpr[3] = sp; 
+    cpu.gpr[4] = 60000;
     cpu.lr = 0xffffffff;
     if(!option.binary){
         assert(lbl.find("_min_caml_start") != lbl.end());
